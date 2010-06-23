86aa99a542a1bbac13ba756c789dfd07514ddd9b
Norman Clarke <norman@njclarke.com>
2010-06-23 18:24:12 -0300

    Cleans up formatting.

--- test/unidecoder_test.rb
+++ test/unidecoder_test.rb
@@ -3,49 +3,50 @@ require "test/unit"
 require File.join(File.dirname(__FILE__), "../init")
 
 class UnidecoderTest < Test::Unit::TestCase
-  # Silly phrases courtesy of Frank da Cruz
-  # http://www.columbia.edu/kermit/utf8.html
+  # Silly phrases courtesy of Frank da Cruz (http://www.columbia.edu/kermit/utf8.html).
+
   DONT_CONVERT = [
-    "Vitrum edere possum; mihi non nocet.", # Latin
-    "Je puis mangier del voirre. Ne me nuit.", # Old French
-    "Kristala jan dezaket, ez dit minik ematen.", # Basque
+    "Vitrum edere possum; mihi non nocet.",               # Latin
+    "Je puis mangier del voirre. Ne me nuit.",            # Old French
+    "Kristala jan dezaket, ez dit minik ematen.",         # Basque
     "Kaya kong kumain nang bubog at hindi ako masaktan.", # Tagalog
-    "Ich kann Glas essen, ohne mir weh zu tun.", # German
-    "I can eat glass and it doesn't hurt me.", # English
+    "Ich kann Glas essen, ohne mir weh zu tun.",          # German
+    "I can eat glass and it doesn't hurt me.",            # English
   ]
+
   CONVERT_PAIRS = {
-    "Je peux manger du verre, ça ne me fait pas de mal." => # French
-      "Je peux manger du verre, ca ne me fait pas de mal.",
-    "Pot să mănânc sticlă și ea nu mă rănește." => # Romanian
-      "Pot sa mananc sticla si ea nu ma raneste.",
-    "Ég get etið gler án þess að meiða mig." => # Icelandic
-      "Eg get etid gler an thess ad meida mig.",
-    "Unë mund të ha qelq dhe nuk më gjen gjë." => # Albanian
-      "Une mund te ha qelq dhe nuk me gjen gje.",
-    "Mogę jeść szkło i mi nie szkodzi." => # Polish
-      "Moge jesc szklo i mi nie szkodzi.",
-    "Я могу есть стекло, оно мне не вредит." => # Russian
-      "Ia moghu iest' stieklo, ono mnie nie vriedit.",
-    "Мога да ям стъкло, то не ми вреди." => # Bulgarian
-      "Mogha da iam stklo, to nie mi vriedi.",
-    "ᛁᚳ᛫ᛗᚨᚷ᛫ᚷᛚᚨᛋ᛫ᛖᚩᛏᚪᚾ᛫ᚩᚾᛞ᛫ᚻᛁᛏ᛫ᚾᛖ᛫ᚻᛖᚪᚱᛗᛁᚪᚧ᛫ᛗᛖ᛬" => # Anglo-Saxon
-      "ic.mag.glas.eotacn.ond.hit.ne.heacrmiacth.me:",
-    "ὕαλον ϕαγεῖν δύναμαι· τοῦτο οὔ με βλάπτει" => # Classical Greek
-      "ualon phagein dunamai; touto ou me blaptei",
-    "मैं काँच खा सकता हूँ और मुझे उससे कोई चोट नहीं पहुंचती" => # Hindi
-      "maiN kaaNc khaa sktaa huuN aur mujhe usse koii cott nhiiN phuNctii",
+    # French
+    "Je peux manger du verre, ça ne me fait pas de mal."     => "Je peux manger du verre, ca ne me fait pas de mal.",
+    # Romanian
+    "Pot să mănânc sticlă și ea nu mă rănește."              => "Pot sa mananc sticla si ea nu ma raneste.",
+    # Icelandic
+    "Ég get etið gler án þess að meiða mig."                 => "Eg get etid gler an thess ad meida mig.",
+    # Albanian
+    "Unë mund të ha qelq dhe nuk më gjen gjë."               => "Une mund te ha qelq dhe nuk me gjen gje.",
+    # Polish
+    "Mogę jeść szkło i mi nie szkodzi."                      => "Moge jesc szklo i mi nie szkodzi.",
+    # Russian
+    "Я могу есть стекло, оно мне не вредит."                 => "Ia moghu iest' stieklo, ono mnie nie vriedit.",
+    # Bulgarian
+    "Мога да ям стъкло, то не ми вреди."                     => "Mogha da iam stklo, to nie mi vriedi.",
+    # Anglo-Saxon
+    "ᛁᚳ᛫ᛗᚨᚷ᛫ᚷᛚᚨᛋ᛫ᛖᚩᛏᚪᚾ᛫ᚩᚾᛞ᛫ᚻᛁᛏ᛫ᚾᛖ᛫ᚻᛖᚪᚱᛗᛁᚪᚧ᛫ᛗᛖ᛬"              => "ic.mag.glas.eotacn.ond.hit.ne.heacrmiacth.me:",
+    # Classical Greek
+    "ὕαλον ϕαγεῖν δύναμαι· τοῦτο οὔ με βλάπτει"              => "ualon phagein dunamai; touto ou me blaptei",
+    # Hindi
+    "मैं काँच खा सकता हूँ और मुझे उससे कोई चोट नहीं पहुंचती" => "maiN kaaNc khaa sktaa huuN aur mujhe usse koii cott nhiiN phuNctii",
+    # Thai
+    "ฉันกินกระจกได้ แต่มันไม่ทำให้ฉันเจ็บ"                   => "chankinkracchkaid aetmanaimthamaihchanecchb",
+    # Chinese
+    "我能吞下玻璃而不伤身体。"                                           => "Wo Neng Tun Xia Bo Li Er Bu Shang Shen Ti . ",
+    # Japanese
+    "私はガラスを食べられます。それは私を傷つけません。"                              => "Si hagarasuwoShi beraremasu. sorehaSi woShang tukemasen. ",
     "من می توانم بدونِ احساس درد شيشه بخورم" => # Persian
       "mn my twnm bdwni Hss drd shyshh bkhwrm",
     "أنا قادر على أكل الزجاج و هذا لا يؤلمن" => # Arabic
       "'n qdr `l~ 'kl lzjj w hdh l yw'lmn",
     "אני יכול לאכול זכוכית וזה לא מזיק לי" => # Hebrew
       "ny ykvl lkvl zkvkyt vzh l mzyq ly",
-    "ฉันกินกระจกได้ แต่มันไม่ทำให้ฉันเจ็บ" => # Thai
-      "chankinkracchkaid aetmanaimthamaihchanecchb",
-    "我能吞下玻璃而不伤身体。" => # Chinese
-      "Wo Neng Tun Xia Bo Li Er Bu Shang Shen Ti . ",
-    "私はガラスを食べられます。それは私を傷つけません。" => # Japanese
-      "Si hagarasuwoShi beraremasu. sorehaSi woShang tukemasen. "
   }
 
   def test_unidecoder_decode
