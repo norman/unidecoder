d8000bc8ae292ba814e3cbeafc0ded39b961494f
Norman Clarke <norman@njclarke.com>
2010-06-23 18:17:57 -0300

    Adds encoding comment for 1.9.

--- bench.rb
+++ bench.rb
@@ -1,9 +1,9 @@
+# encoding: utf-8
 require "rubygems"
 require "rbench"
 require File.expand_path('../lib/unidecoder', __FILE__)
 
 SAMPLE = "Ja, żołnierz Wojska Polskiego, przysięgam służyć wiernie Rzeczypospolitej Polskiej"
-p SAMPLE.to_ascii
 
 RBench.run(10_000) do
 
