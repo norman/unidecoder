7c07c7577120076fb8e9062795bff96b34b40e4c
Norman Clarke <norman@njclarke.com>
2010-06-23 18:43:17 -0300

    Bumps version.

--- lib/unidecoder/version.rb
+++ lib/unidecoder/version.rb
@@ -1,8 +1,8 @@
 module Unidecoder
   module Version
     MAJOR = 1
-    MINOR = 0
-    TINY = 1
+    MINOR = 1
+    TINY = 0
     BUILD = nil
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
   end
